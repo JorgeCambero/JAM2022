bf4b1a33fde5da1edf200e0a410d76a14540f5b1
Whikkot <whikko20@gmail.com>
2022-02-05 00:32:55 +0100

    Se ha añadido movimiento lateral al personaje
    Una primera toma de contacto con el código.

--- jam2022/Assets/Scripts/PlayerMov.cs
+++ jam2022/Assets/Scripts/PlayerMov.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMov : MonoBehaviour
 {
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("left") || Input.GetKey("a"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-2000f * Time.deltaTime, 0));
+            if (rb.velocity.x <= -15) { rb.velocity = new Vector2(-15f, rb.velocity.y); }
+        }
+        //right
+        if (Input.GetKey("right") || Input.GetKey("d"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(2000f * Time.deltaTime, 0));
+            if (rb.velocity.x >= 15) { rb.velocity = new Vector2(15f, rb.velocity.y); }
+        }
     }
 }
