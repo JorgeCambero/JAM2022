b7ada4b96f4459b48321810a0239e5aba38f1d86
Whikkot <whikko20@gmail.com>
2022-02-05 17:41:44 +0100

    Añadida congelación

--- jam2022/Assets/Scripts/pauseW.cs
+++ jam2022/Assets/Scripts/pauseW.cs
@@ -7,12 +7,32 @@ public class pauseW : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Rigidbody2D>() != null)
+        {
+
+            collision.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+            //collision.GetComponent<Rigidbody2D>().constraints = ;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Rigidbody2D>() != null)
+        {
+            collision.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+            collision.GetComponent<Rigidbody2D>().WakeUp();
+            //collision.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
     }
 }
+
