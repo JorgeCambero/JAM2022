83f649b15bef1a949042e6b4b81958687e134253
Whikkot <whikko20@gmail.com>
2022-02-08 22:18:34 +0100

    FIN DEL JUEGO Y DIALOGOS, POG

--- jam2022/Assets/Scripts/AudioManager/AudioManager/AudioManager.cs
+++ jam2022/Assets/Scripts/AudioManager/AudioManager/AudioManager.cs
@@ -2,6 +2,7 @@ using UnityEngine.Audio;
 using System;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
 public class AudioManager : MonoBehaviour
 {
 
@@ -9,8 +10,21 @@ public class AudioManager : MonoBehaviour
 
 	public AudioMixerGroup mixerGroup;
 
-	public Sound[] sounds;
 
+	public bool cambiar;
+
+	Scene currentScene; 
+	public Sound[] sounds;
+	void Start() 
+	{
+		currentScene = SceneManager.GetActiveScene();
+		Debug.Log(currentScene.name);
+		if (currentScene.name == "MenuInicio")
+		{
+			Play("Menu");
+		}
+		
+	}
 	void Awake()
 	{
 		if (instance != null)
@@ -48,4 +62,48 @@ public class AudioManager : MonoBehaviour
 		s.source.Play();
 	}
 
+	public void StopPlaying(string sound)
+	{
+		Sound s = Array.Find(sounds, item => item.name == sound);
+		if (s == null)
+		{
+			Debug.LogWarning("Sound: " + name + " not found!");
+			return;
+		}
+
+		s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+		s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+		s.source.Stop();
+	}
+	private void Update()
+	{
+		if (cambiar)
+		{
+			currentScene = SceneManager.GetActiveScene();
+			Debug.Log(cambiar);
+			Debug.Log(currentScene.name);
+			cambiar = false;
+			if (currentScene.name == "tuto1")
+			{
+				StopPlaying("Menu");
+				Play("Tutorial");
+			}
+			else if (currentScene.name == "lvl3")
+			{
+				StopPlaying("Tutorial");
+				Play("Facil");
+			}
+			else if (currentScene.name == "nivelJordan")
+			{
+				StopPlaying("Facil");
+				Play("Dificil");
+			}
+			else if (currentScene.name == "Ending")
+			{
+				StopPlaying("Dificil");
+				Play("Final");
+			}
+		}
+	}
 }
