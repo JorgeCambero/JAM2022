0824550a4087dca9ebde7167f6763e352499a0b2
Whikkot <whikko20@gmail.com>
2022-02-08 14:52:03 +0100

    Casi todo el arte
    Faltan las puertas

--- jam2022/Assets/Scripts/cambiarNivel.cs
+++ jam2022/Assets/Scripts/cambiarNivel.cs
@@ -7,21 +7,30 @@ public class cambiarNivel : MonoBehaviour
 {
     // Start is called before the first frame update
     public string escenaSiguiente;
+    public bool activo;
+    Animator animations = new Animator();
     void Start()
     {
-        
+        animations = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (activo)
+        {
+            animations.SetBool("encendido", true);
+        }
+        else
+        {
+            animations.SetBool("encendido", false);
+        }
     }
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Cambiando nivel...");
 
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Player") || collision.gameObject.layer == LayerMask.NameToLayer("PlayerW"))
+        if ((collision.gameObject.layer == LayerMask.NameToLayer("Player") || collision.gameObject.layer == LayerMask.NameToLayer("PlayerW")) && activo)
         {
             SceneManager.LoadScene(escenaSiguiente);
         }
